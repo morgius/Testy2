ae514a79d53a58c5e32e501d7a68bb09c8697769
Paweł Podsiadło <p.podsiadlo@microsoft.wsei.edu.pl>
2019-11-05 18:29:38 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Testy2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
