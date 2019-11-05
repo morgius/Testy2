4db76cad349e9bce487ee9a06116ccf8b2ee22b5
Paweł Podsiadło <p.podsiadlo@microsoft.wsei.edu.pl>
2019-11-05 20:13:23 +0100

    koniec zajęć

---
+++ OrderService.cs
@@ -0,0 +1,38 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Testy2
+{
+    public class OrderService
+    {
+        private readonly IStorage _storage;
+
+        public OrderService(IStorage storage)
+        {
+            _storage = storage;
+        }
+        public int PlaceOrder(Order order)
+        {
+            try
+            {
+                var orderId = _storage.Store(order);
+                return orderId *3;
+            }
+            catch (ArgumentNullException)
+            {
+                return -1;
+            }
+            // Some other work
+        }
+    }
+    public class Order
+    {
+    }
+    public interface IStorage
+    {
+        int Store(Order order);
+        Order Exists(int id);
+    }
+}
